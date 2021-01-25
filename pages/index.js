d32e1cad7d56198ef348dd2f0e3a45c0b89f4983
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-01-25 14:28:51 +0100

    OG once again

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,51 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Corona-Ampel App - Aktuelle Zahlen, Inzidenz, Impf-Fortschritt ...</title>
-        <meta name="description" content="Alle Landkreise, Bundesländer im Überblick. Die neue Corona-Ampel App hat die wichtigsten Corona und Covid-19 Daten für ganz Deutschland in übersichtlicher Form!" />
-        <meta property="og:title" content="Corona-Ampel App - Aktuelle Zahlen, Inzidenz, Impf-Fortschritt ..." />
+        <title>
+          Corona-Ampel App - Aktuelle Zahlen, Inzidenz, Impf-Fortschritt ...
+        </title>
+        <meta
+          name="description"
+          content="🚦 Alle Landkreise, Bundesländer im Überblick. Die neue Corona-Ampel App hat die wichtigsten Corona und Covid-19 Daten für ganz Deutschland in übersichtlicher Form!"
+        />
+        <meta
+          property="og:title"
+          content="Corona-Ampel App - Aktuelle Zahlen, Inzidenz, Impf-Fortschritt ..."
+        />
         <meta property="og:url" content="https://corona-ampel.app" />
-        <meta property="og:description" content="Alle Landkreise, Bundesländer im Überblick. Die neue Corona-Ampel App hat die wichtigsten Corona und Covid-19 Daten für ganz Deutschland in übersichtlicher Form!" />
-        <meta property="og:image" content="https://corona-ampel.app/app-icon.png" />
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://corona-ampel.app/app-icon.png" rel="icon" type="image/png"></link>
+        <meta
+          property="og:description"
+          content="🚦 Alle Landkreise, Bundesländer im Überblick. Die neue Corona-Ampel App hat die wichtigsten Corona und Covid-19 Daten für ganz Deutschland in übersichtlicher Form!"
+        />
+        <meta
+          property="og:image"
+          content="https://corona-ampel.app/app-icon.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="de_DE" />
+        <meta
+          name="twitter:image"
+          content="https://corona-ampel.app/app-icon.png"
+        />
+        <meta name="”twitter:creator”" value="”@ingomc”" />
+
+        <link rel="icon" href="https://corona-ampel.app/favicon.ico" />
+        <link
+          href="https://corona-ampel.app/app-icon.png"
+          rel="icon"
+          type="image/png"
+        ></link>
       </Head>
 
       <main className={styles.main}>
         <img src="/app-icon.png" className={styles.logo} />
-        <h1 className={styles.title}>
-          Corona-Ampel für Android und iOS
-        </h1>
+        <h1 className={styles.title}>Corona-Ampel für Android und iOS</h1>
         <h2>Coming soon ...</h2>
       </main>
     </div>
-  )
+  );
 }
