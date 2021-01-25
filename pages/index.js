b3f02a9fb2a8e52cf8ea561f4b091df9b9d35b79
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-01-25 14:15:00 +0100

    again OG image

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <meta property="og:title" content="Corona-Ampel App - Aktuelle Zahlen, Inzidenz, Impf-Fortschritt ..." />
         <meta property="og:url" content="https://corona-ampel.app" />
         <meta property="og:description" content="Alle Landkreise, Bundesländer im Überblick. Die neue Corona-Ampel App hat die wichtigsten Corona und Covid-19 Daten für ganz Deutschland in übersichtlicher Form!" />
-        <meta property="og:image" content="/app-icon.png" />
+        <meta property="og:image" content="https://corona-ampel.app/app-icon.png" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://corona-ampel.app/app-icon.png" rel="icon" type="image/png"></link>
       </Head>
